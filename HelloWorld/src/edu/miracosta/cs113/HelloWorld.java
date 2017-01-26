81f9428635dd17bdefc7f2c2ca245f8b9fdef5ef
Sina Dalir <sinahdalir14@gmail.com>
2017-01-26 11:20:20 -0800

    testing text files and package creation
    created a new text file to see how theyre made, as well as a new package
    and how to access it inside of another class

--- HelloWorld/src/edu/miracosta/cs113/HelloWorld.java
+++ HelloWorld/src/edu/miracosta/cs113/HelloWorld.java
@@ -1,10 +1,11 @@
 package edu.miracosta.cs113;
 
+import testPackage.Test;
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
+		Test.testMe();
 	}
 
 }
---
+++ HelloWorld/src/testPackage/Test.java
@@ -0,0 +1,9 @@
+package testPackage;
+
+public class Test {
+
+	public static void testMe(){
+		
+		//doesnt do anything
+	}
+}
